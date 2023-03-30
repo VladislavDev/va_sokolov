e16de5283a45526698dfa54191eb650c5bb45474
Vladislav Sokolov <sokolov.vladislavdev@gmail.com>
2023-03-30 21:45:40 +0700

    Feed images views was created

--- feeds/admin.py
+++ feeds/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 
-from .models import Feed
+from .models import Feed, FeedImage
 
-admin.site.register(Feed)
+class FeedImageInline(admin.StackedInline):
+    model = FeedImage
+    extra = 0
+    
+class FeedsAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {"fields": ('text',)}),
+        ('Publication', {"fields": ('pub_date', 'author')}),
+    )
+    inlines = [FeedImageInline]
+    
+
+admin.site.register(Feed, FeedsAdmin)
--- feeds/models.py
+++ feeds/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.core.files.images import get_image_dimensions
+
 from userprofile.models import Profile
 
 
@@ -11,8 +13,40 @@ class Feed(models.Model):
     def __str__(self):
         return str(self.pub_date) + ' ' + self.text
     
+    @property
+    def image_rows(self):
+        
+        if self.images.count() == 0:
+            return None
+        
+        image_rows_groupping_template = [
+            [1], [2], [1, 2], [2, 2], 
+            [2, 3], [2, 4], [3, 4], [2, 6],
+            [3, 3, 3], [3, 3, 4]
+        ]
+        image_rows_template = image_rows_groupping_template[self.images.count()-1]
+        
+        rows = [ {'images': [] } for i in range(len(image_rows_template))]
+        
+        # Images groupping in rows
+        for image in self.images.all():
+            for i, row in enumerate(image_rows_template):
+                if row > 0:
+                    width, height = get_image_dimensions(image.image.file)
+                    
+                    rows[i]['images'].append({
+                        'url': image.image.url,
+                        'grow': width / height
+                    })
+                    
+                    image_rows_template[i] -= 1
+                    break
+        
+        return rows
+                    
+                    
     
 class FeedImage(models.Model):
     
     image = models.ImageField(upload_to='feeds-images')
-    feed = models.ForeignKey(Feed, on_delete=models.CASCADE)
+    feed = models.ForeignKey(Feed, on_delete=models.CASCADE, related_name='images')
